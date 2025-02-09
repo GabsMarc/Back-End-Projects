f4e81bcbf1f1a6e090665b065235302668bf5b64
Gabriel Marcucci <gamarcucci@gmail.com>
2025-02-09 18:20:48 -0300

    feat: commit inicial

---
+++ API-Node-e-Typescript/src/server/server.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response } from 'express'
+
+const server = express()
+
+server.get('/', (req: Request, res: Response) => {
+    res.send('Olá, Dev!')
+})
+
+
+
+
+export { server }
