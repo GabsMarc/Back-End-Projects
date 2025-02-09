eba6c26fbcbd9e9f252391da86b31340c44d0b37
Gabriel Marcucci <gamarcucci@gmail.com>
2025-02-09 17:45:48 -0300

    Commit

---
+++ API-Node-e-Typescript/src/server/Server.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response } from "express"
+
+
+
+const server = express()
+
+
+server.get('/', (req: Request, res: Response) => {
+   res.send('Olá, Dev!')
+})
+
+
+
+export { server }
