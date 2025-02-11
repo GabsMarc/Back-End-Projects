3e764e66017ebd2833eb64d2336f03ed6fc37713
Gabriel Marcucci <gamarcucci@gmail.com>
2025-02-10 21:08:33 -0300

    Commit

--- API-Node-e-Typescript/src/index.ts
+++ API-Node-e-Typescript/src/index.ts
@@ -1,5 +1,5 @@
 import { server } from './server/server'
 
 server.listen(process.env.PORT || 3333, () =>
-  console.log(`Entrou na rota ${process.env.PORT || 3333}`),
+  console.log(`Entrou na rota ${process.env.PORT || 3333}`)
 )
